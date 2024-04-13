479298dca8dc13c9db0e471aafef026a13958eb7
joel oluwafisayo <oluwafisayojoel@gmail.com>
2024-04-13 10:43:02 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { useAppSelector } from "@/redux/store";
+
 import React from "react";
+// import { HomeImage } from "./assets/images/homepageimage.png";
 
 // import "../styles/global.css";
 
 export default function Home() {
   const username = useAppSelector((state) => state.authReducer.value.userName);
   return (
-    <div>
-      <p className="homepage">pagesss - username:{username}</p>
+    <div className="homepage">
+      {/* <p className="homepage">pagesss - username:{username}</p> */}
       <div className="homepage_info">
-        <div></div>
+        <div className="homepage_infoimage">ssssss</div>
         <div></div>
       </div>
       <div className="homepage_login"></div>
