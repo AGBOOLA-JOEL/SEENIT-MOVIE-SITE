66d83272625fd6e570496d422931fb19ba09d5a1
joel oluwafisayo <oluwafisayojoel@gmail.com>
2024-04-16 12:22:52 +0100

    poster fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,30 @@ export default function Home() {
     <div className="homepage">
       {/* <p className="homepage">pagesss - username:{username}</p> */}
       <div className="homepage_info">
-        <div className="homepage_infoimage">
-          ssssss
-          <Image
-            src="/public/assets/images/homepageimage.png"
-            alt="image"
-            width={100}
-            height={100}
-          />
+        <div className="homepage_infowrapper">
+          <div className="homepage_infoimage">
+            {/* <div className="homepage_infoimagemain"> */}
+            <Image
+              src="/assets/images/homepagejoin.png"
+              alt="image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "90%", borderRadius: 8 }}
+            />
+           
+
+            <div className="homepage_infotext">
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+            </div>
+          </div>
+          <div></div>
         </div>
-        <div></div>
       </div>
       <div className="homepage_login"></div>
     </div>
